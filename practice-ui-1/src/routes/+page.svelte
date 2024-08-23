30d884e6acc2029db0309cb85ebc4cf2026066e1
Justin Garner <charlottewebsolutions1@gmail.com>
2024-08-22 22:07:55 -0400

    Create basic layout

--- practice-ui-1/src/routes/+page.svelte
+++ practice-ui-1/src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    //import hillImg from "$lib/assets/capture.png";
+</script>
+
+<div class="w-screen h-screen flex flex-col">
+    <div class="w-full h-1/2">
+        <!--test-->
+        <img class="h-full" src="" alt="">
+    </div>
+
+    <div class="flex h-1/2 w-full">
+        <div class="w-2/3 h-full">
+
+        </div>
+        <div class="w-1/3 h-full bg-[#F9BBCB]">
+
+        </div>
+    </div>
+</div>
